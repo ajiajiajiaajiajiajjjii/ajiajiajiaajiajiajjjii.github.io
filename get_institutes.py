1f82049fbeb7345f938fd28a9a37534cec8acfc9
andreapi9 <136931558+andreapi9@users.noreply.github.com>
2024-01-04 00:04:39 +0100

    Added file to scrape the list of reserach institutes and aree disciplinari. Added folder with support files.

---
+++ get_institutes.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+
+# Read the HTML file
+with open("downloaded_html/cercaFellowship", "r", encoding="utf-8") as f:
+    html = f.read()
+
+# Parse the HTML using BeautifulSoup
+soup = BeautifulSoup(html, "html.parser")
+
+# Extract all the options from the HTML file
+options = []
+for select in soup.find_all("select"):
+    options.extend([option.text for option in select.find_all("option")])
+
+# Remove leading and trailing whitespace characters and newline characters
+options = [option.strip().replace("\n", "") for option in options]
+
+# Remove the first 4 elements of the list (referring to Aperti, Scaduti, etc)
+options = options[4:]
+
+# Find the index of the first occurrence of "Tutti", to separate the institutes from the Aree Disciplinari
+index = options.index("Tutti")
+
+
+institutes = options[:index]                #List with Institutes
+areedisciplinari = options[index+1:]        #List with Aree Disciplinari
+
+# Write the options to text files
+with open("support_files/options_institutes.txt", "w", encoding="utf-8") as f:
+    f.write("\n".join(institutes))
+
+with open("support_files/options_aree_settoriali.txt", "w", encoding="utf-8") as f:
+    f.write("\n".join(areedisciplinari))
